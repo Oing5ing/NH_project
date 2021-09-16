038f917512fdd8e326396acf93ca558324bd73d7
EC2 Default User <ec2-user@ip-172-31-14-228.ap-northeast-2.compute.internal>
2021-09-16 02:15:30 +0000

    add gokmuli.py

--- gokmuli.py
+++ gokmuli.py
@@ -7,6 +7,7 @@ import sys
 import pymysql
 import pandas as pd
 import numpy as np
+import re
 
 db = pymysql.connect(host='localhost',
                      port=3306,
@@ -24,15 +25,13 @@ app = Flask(__name__)
 # 나이
 @app.route('/age', methods=['POST'])
 def Age():
-
     content = request.get_json()
-
     # 사용자 고유 id
-    user_id = content['userRequest']['user']['properties']['plusfriendUserKey']   
+    user_id = content['userRequest']['user']['properties']['botUserKey']
     # 나이 입력값
     content = content['action']
     content = content['params']
-    content = int(content['age'])
+    content = int(re.sub('[^0-9]', '', content['age']))
 
     # DB로 전송
     cursor = db.cursor()
@@ -42,20 +41,9 @@ def Age():
 
     dataSend = {
         "version": "2.0",
-        "template": {
-            "outputs": [
-                {
-                    "carousel": {
-                        "type" : "basicCard",
-                        "items": [
-                            {
-                                "title" : "",
-                                "description" : "열량은 얼마나 섭취하고 싶으신가요?\n(100g 기준. 97~358kcal 사이의 값을 입력해주세요.)"                            }
-                        ]
-                    }
-                }
-            ]
-        }
+        "data": {
+            "age": content
+            }
     }
     return jsonify(dataSend)
 
@@ -65,7 +53,7 @@ def Calory():
 
     content = request.get_json()
     # 사용자 고유 id
-    user_id = content['userRequest']['user']['properties']['plusfriendUserKey']
+    user_id = content['userRequest']['user']['properties']['botUserKey']
     # 사용자 입력값
     content = content['action']
     content = content['params']
@@ -80,23 +68,12 @@ def Calory():
 
     dataSend = {
         "version": "2.0",
-        "template": {
-            "outputs": [
-                {
-                    "carousel": {
-                        "type" : "basicCard",
-                        "items": [
-                            {
-                                "title" : "",
-                                "description" : "탄수화물은 얼마나 섭취하고 싶으신가요?\n(100g 기준. 21.09~78.84g사이의 값을 입력해주세요.)"
-                            }
-                        ]
-                    }
-                }
-            ]
-        }
+        "data": {
+            "calory": content
+            }
     }
     return jsonify(dataSend)
+
 
 # 탄수화물
 @app.route('/carbohydrate', methods=['POST'])
@@ -104,7 +81,7 @@ def Carbohydrate():
 
     content = request.get_json()
     # 사용자 고유 id
-    user_id = content['userRequest']['user']['properties']['plusfriendUserKey']
+    user_id = content['userRequest']['user']['properties']['botUserKey']
     # 사용자 입력값
     content = content['action']
     content = content['params']
@@ -119,21 +96,9 @@ def Carbohydrate():
 
     dataSend = {
         "version": "2.0",
-        "template": {
-            "outputs": [
-                {
-                    "carousel": {
-                        "type" : "basicCard",
-                        "items": [
-                            {
-                                "title" : "",
-                                "description" : "단백질은 얼마나 섭취하고 싶으신가요?\n(100g 기준. 2.02~8.52g 사이의 값을 입력해주세요.)"
-                            }
-                        ]
-                    }
-                }
-            ]
-        }
+        "data": {
+            "carbohydrate": content
+            }
     }
     return jsonify(dataSend)
 
@@ -143,7 +108,7 @@ def Protein():
 
     content = request.get_json()
     # 사용자 고유 id
-    user_id = content['userRequest']['user']['properties']['plusfriendUserKey']
+    user_id = content['userRequest']['user']['properties']['botUserKey']
     # 사용자 입력값
     content = content['action']
     content = content['params']
@@ -158,21 +123,9 @@ def Protein():
 
     dataSend = {
         "version": "2.0",
-        "template": {
-            "outputs": [
-                {
-                    "carousel": {
-                        "type" : "basicCard",
-                        "items": [
-                            {
-                                "title" : "",
-                                "description" : "지방은 얼마나 섭취하고 싶으신가요?\n(100g 기준. 0.19~1.06g 사이의 값을 입력해주세요.)"
-                            }
-                        ]
-                    }
-                }
-            ]
-        }
+        "data": {
+            "protein": content
+            }
     }
     return jsonify(dataSend)
 
@@ -182,7 +135,7 @@ def Fat():
 
     content = request.get_json()
     # 사용자 고유 id
-    user_id = content['userRequest']['user']['properties']['plusfriendUserKey']
+    user_id = content['userRequest']['user']['properties']['botUserKey']
     # 사용자 입력값
     content = content['action']
     content = content['params']
@@ -197,21 +150,9 @@ def Fat():
 
     dataSend = {
         "version": "2.0",
-        "template": {
-            "outputs": [
-                {
-                    "carousel": {
-                        "type" : "basicCard",
-                        "items": [
-                            {
-                                "title" : "",
-                                "description" : "당류는 얼마나 섭취하고 싶으신가요?\n(100g 기준. 0~0.46g 사이의 값을 입력해주세요.)"
-                            }
-                        ]
-                    }
-                }
-            ]
-        }
+        "data": {
+            "fat": content
+            }
     }
     return jsonify(dataSend)
 
@@ -221,7 +162,7 @@ def Sugars():
 
     content = request.get_json()
     # 사용자 고유 id
-    user_id = content['userRequest']['user']['properties']['plusfriendUserKey']
+    user_id = content['userRequest']['user']['properties']['botUserKey']
     # 사용자 입력값
     content = content['action']
     content = content['params']
@@ -236,21 +177,9 @@ def Sugars():
 
     dataSend = {
         "version": "2.0",
-        "template": {
-            "outputs": [
-                {
-                    "carousel": {
-                        "type" : "basicCard",
-                        "items": [
-                            {
-                                "title" : "",
-                                "description" : "식이섬유는 얼마나 섭취하고 싶으신가요?\n(100g 기준. 0.1~8.4g 사이의 값을 입력해주세요.)"
-                            }
-                        ]
-                    }
-                }
-            ]
-        }
+        "data": {
+            "sugars": content
+            }
     }
     return jsonify(dataSend)
 
@@ -260,7 +189,7 @@ def DietaryFiber():
 
     content = request.get_json()
     # 사용자 고유 id
-    user_id = content['userRequest']['user']['properties']['plusfriendUserKey']
+    user_id = content['userRequest']['user']['properties']['botUserKey']
     # 사용자 입력값
     content = content['action']
     content = content['params']
@@ -275,21 +204,9 @@ def DietaryFiber():
 
     dataSend = {
         "version": "2.0",
-        "template": {
-            "outputs": [
-                {
-                    "carousel": {
-                        "type" : "basicCard",
-                        "items": [
-                            {
-                                "title" : "",
-                                "description" : "나트륨은 얼마나 섭취하고 싶으신가요?\n(100g 기준. 0.001~0.459g 사이의 값을 입력해주세요.)"
-                            }
-                        ]
-                    }
-                }
-            ]
-        }
+        "data": {
+            "dietaryfiber": content
+            }
     }
     return jsonify(dataSend)
 
@@ -299,7 +216,7 @@ def Sodium():
 
     content = request.get_json()
     # 사용자 고유 id
-    user_id = content['userRequest']['user']['properties']['plusfriendUserKey']
+    user_id = content['userRequest']['user']['properties']['botUserKey']
     # 사용자 입력값
     content = content['action']
     content = content['params']
@@ -329,21 +246,27 @@ def Sodium():
 
     dataSend = {
         "version": "2.0",
-        "template": {
-            "outputs": [
-                {
-                    "carousel": {
-                        "type" : "basicCard",
-                        "items": [
-                            {
-                                "title" : "",
-                                "description" : "모든 응답을 완료했습니다.\n현재 선택하신 내용은 "+ "\n나이: " + str(user_input[0][0])+'세'+"\n열량: " + str(user_input[0][1])+'kcal'+"\n탄수화물: " + str(user_input[0][2])+'g'+ "\n단백질: " + str(user_input[0][3])+'g'+"\n지방: " + str(user_input[0][4])+'g'+"\n당류: " + str(user_input[0][5])+'g'+"\n식이섬유: " + str(user_input[0][6])+'g'+"\n나트륨: " + str(user_input[0][7])+'g'+"\n입니다.\n선택하신 내용에 맞게 추천된 블랜딩 비율은 \n 흑미 " + str(np.array(grain_percent)[0][0])  + " % \n 현미 " + str(np.array(grain_percent)[0][1]) + "% \n 보리 " + str(np.array(grain_percent)[0][2]) + "% \n 차조 " + str(np.array(grain_percent)[0][3]) + "% \n 기장 " + str(np.array(grain_percent)[0][4]) + "% \n 율무 " + str(np.array(grain_percent)[0][5]) + "% \n 수수 " + str(np.array(grain_percent)[0][6]) + "% \n 찹쌀 " + str(np.array(grain_percent)[0][7]) + "% \n 적두 " + str(np.array(grain_percent)[0][8]) + "% \n 서리태 " + str(np.array(grain_percent)[0][9]) + "% \n 입니다."                           }
-                        ]
-                    }
-                }
-                        ]
-                    }
-                }
+        "data": {
+            "age": user_input[0][0],
+            "calory": user_input[0][1],
+            "carbohydrate": user_input[0][2],
+            "protein": user_input[0][3],
+            "fat": user_input[0][4],
+            "sugars": user_input[0][5],
+            "dietaryfiber": user_input[0][6],
+            "sodium": user_input[0][7],
+            "heugmi": str(np.round(np.array(grain_percent)[0][0], 2)),
+            "hyeonmi": str(np.round(np.array(grain_percent)[0][1], 2)),
+            "boli": str(np.round(np.array(grain_percent)[0][2], 2)),
+            "chajo": str(np.round(np.array(grain_percent)[0][3], 2)),
+            "gijang": str(np.round(np.array(grain_percent)[0][4], 2)),
+            "yulmu": str(np.round(np.array(grain_percent)[0][5], 2)),
+            "susu": str(np.round(np.array(grain_percent)[0][6], 2)),
+            "chabssal": str(np.round(np.array(grain_percent)[0][7], 2)),
+            "jeogdu": str(np.round(np.array(grain_percent)[0][8], 2)),
+            "seolitae": str(np.round(np.array(grain_percent)[0][9], 2))
+        }
+    }
     return jsonify(dataSend)
 
 
